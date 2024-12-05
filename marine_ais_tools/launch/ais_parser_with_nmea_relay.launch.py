027b1bbe9004a2762ad0e8f8bb52c4d48ac91414
jspruit <johnspruit@gmail.com>
2024-12-05 10:55:36 +0000

    Move parameters to .yaml file

--- marine_ais_tools/launch/ais_parser_with_nmea_relay.launch.py
+++ marine_ais_tools/launch/ais_parser_with_nmea_relay.launch.py
@@ -1,7 +1,17 @@
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
+    config = PathJoinSubstitution([
+        get_package_share_directory("marine_ais_tools"),
+        "config",
+        "parameters.yaml"
+    ])
+
     nmea_relay = Node(
         package="marine_ais_tools",
         executable="nmea_relay",
@@ -9,6 +19,7 @@ def generate_launch_description():
         remappings=[
             ("/nmea", "/ais/raw"),
         ],
+        parameters=[config]
     )
 
     ais_parser = Node(
